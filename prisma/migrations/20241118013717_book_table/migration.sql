0754ac22f378dd9df4869b1c897407f4d9b05c57
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-18 07:38:39 +0600

    create-book-shema

---
+++ prisma/migrations/20241118013717_book_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "book" (
+    "bookId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "totalCopies" INTEGER NOT NULL,
+    "availableCopies" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "book_pkey" PRIMARY KEY ("bookId")
+);
